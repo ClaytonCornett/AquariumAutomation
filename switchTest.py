9e5b176aba44ea4571b2090e89ffe26dd9e759e7
Clayton Cornett <claytoncornett@Claytons-MacBook-Pro-2.local>
2020-03-23 16:42:04 -0500

    added file for testing the switch

---
+++ switchTest.py
@@ -0,0 +1,53 @@
+import time
+import RPi.GPIO as GPIO
+import os
+import glob
+import time
+
+time.sleep(2)
+
+GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
+ 
+RELAIS_1_GPIO = 17
+GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) #Relay 4, Heater
+GPIO.setup(RELAIS_1_GPIO, GPIO.LOW) #starts heater on
+
+RELAY_3 = 27
+GPIO.setup(RELAY_3, GPIO.OUT) #Relay 3, light
+GPIO.output(RELAY_3, GPIO.LOW) #Starts light on
+
+RELAY_2 = 22
+GPIO.setup(RELAY_2, GPIO.OUT) #Relay 2, Non custom feeder
+mealsPerDay = 2
+mealDaysRemaining = 7  #These vars keep track of when to turn off feeder
+nonCustomFeederNeeded = True #Will not be used until needed
+nonCustomFeederOn = False
+
+
+try:
+    while True:
+        time.sleep(1)
+        GPIO.output(17, GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(17, GPIO.HIGH)
+        
+        time.sleep(1)
+        GPIO.output(27, GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(27, GPIO.HIGH)
+        
+        time.sleep(1)
+        GPIO.output(22, GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(22, GPIO.HIGH)
+
+except KeyboardInterrupt:
+    print("Keyboard Interrupt ended script")
+
+except:
+    print("Other exception occured")
+
+finally:
+    GPIO.cleanup() #clean exit
+
+
