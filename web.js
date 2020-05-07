7f5faeb9f8f0fb792142209a9e33e3670250879e
ClaytonCornett <claytoncornett3@gmail.com>
2020-05-07 15:39:21 -0500

    Not returning the json data to webpage

--- web.js
+++ web.js
@@ -3,20 +3,21 @@ var lightsOn = "";
 var heaterOn = "";
 var feederOn = "";
 
-function fetchdata() {
-	var request = new HMLHttpRequest();
+	var request = new XMLHttpRequest();
 
-	request.open('GET', "localhost/data.txt");
+	request.open('GET', "data.json");
 	request.onload = function(){
 		var data = JSON.parse(request.responseText);
 		temp = data["aquarium"][0];
+		console.log(temp)
 		lightsOn = data["aquarium"][1];
+		console.log(lightsOn)
 		heaterOn = data["aquarium"][2];
+		console.log(heaterOn)
 		feederOn = data["aquarium"][3];
+		console.log(feederOn)
 		document.getElementById('test').innerHTML = 
-		"Temp: " +  temp + "<br>" +
-		"Lights: " + lightsOn + "<br>" +
-		"Heater: " + heaterOn + "<br>" +
-		"Feeder: " + feederOn + "<br>";
+		"Temp: " +  lightsOn;
+		console.log("at the end")
 	}
-}
+
